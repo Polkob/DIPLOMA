90da275400022738439ef61021b2e57dee0e05c7
polina <polyasuper2.0@gmail.com>
2025-03-26 12:06:07 +0300

    header

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import IconUser from './components/icons/IconUser.vue';
+import IconFavorite from './components/icons/IconFavorite.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <h1>FilmFinder</h1>
+    <button class="button-icon"><IconFavorite /></button>
+    <button class="button-icon"><IconUser /></button>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+header {
+  background-color: var(--vt-c-black-mute);
+  display: grid;
+  grid-template-columns: 90% 5% 5%;
+  padding: 1rem;
+  width: 100vw;
+  border-bottom: 2px solid var(--vt-c-blue-dark);
+  box-shadow: 0 10px 10px -10px #440087;
+}
+h1 {
+  color: white;
+}
+
+.button-icon{
+  background-color: inherit;
+  border: none;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+
+.button-icon:hover {
+  background-color:  var(--vt-c-black-mute-2);
+}
+</style>
---
+++ src/components/icons/IconUser.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg class="icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <title />
+    <circle cx="12" cy="8" r="4" fill="gray" />
+    <defs>
+      <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="100%">
+        <stop offset="0%" style="stop-color: #9a5aff; stop-opacity: 1" />
+        <stop offset="100%" style="stop-color: #87edff; stop-opacity: 1" />
+      </linearGradient>
+    </defs>
+    <path
+      d="M20,19v1a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V19a6,6,0,0,1,6-6h4A6,6,0,0,1,20,19Z"
+      fill="gray"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "IconUser",
+};
+</script>
+
+<style scoped>
+.icon {
+    width: 35px;
+    height: 35px;
+}
+</style>

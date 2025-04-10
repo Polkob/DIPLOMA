f800785440fc6e69b57ce2858daebdf59f006d79
polina <polyasuper2.0@gmail.com>
2025-04-10 21:00:58 +0300

    template main

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,18 @@ import IconFavorite from './components/icons/IconFavorite.vue';
   </header>
 
   <main>
-    
+    <section class="panel">
+      <input type="text" placeholder="Поиск фильма..." class="search-input" />
+
+      <div class="cards">
+        <div class="movie-card">
+          <img src="https://via.placeholder.com/100x150?text=Poster+1" alt="Movie Poster 1" />
+        </div>
+        <div class="movie-card">
+          <img src="https://via.placeholder.com/100x150?text=Poster+2" alt="Movie Poster 2" />
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -42,4 +53,42 @@ h1 {
 .button-icon:hover {
   background-color:  var(--vt-c-black-mute-2);
 }
+.panel {
+  background-color: #1a1a1a;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  border-bottom: 1px solid var(--vt-c-blue-dark);
+  box-shadow: 0 5px 5px -5px #440087;
+}
+
+.search-input {
+  flex: 1;
+  padding: 0.5rem 1rem;
+  border-radius: 8px;
+  border: none;
+  font-size: 1rem;
+  outline: none;
+}
+
+.cards {
+  display: flex;
+  gap: 1rem;
+}
+
+.movie-card {
+  width: 100px;
+  height: 150px;
+  overflow: hidden;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.4);
+}
+
+.movie-card img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
 </style>
